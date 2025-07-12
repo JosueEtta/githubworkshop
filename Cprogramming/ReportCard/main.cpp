345463136d21939abfed60e60b5a8c29029c2707
JosueEtta <atabejosue@gmail.com>
2025-07-12 11:18:20 +0100

    Completed report card as given exercise

--- Cprogramming/ReportCard/main.cpp
+++ Cprogramming/ReportCard/main.cpp
@@ -3,42 +3,80 @@
 
 using namespace std;
 
+
+class Course{
+   public:
+   string courseName;
+   int coef;
+   float courseMark;
+
+   //Course()
+};
+
 class Student{
-   Course* courses;
    public:
     string name;
     int mathScore;
     char grade;
     int courseOffer;
+    string gender;
+    Course* courses;
 
-    Student(string name,int courseOffer){
+
+    Student(string name={0},int courseOffer=0,string gender={0}){
       this->name = name;
       this->courseOffer = courseOffer;
+      this->gender = gender;
     }
-    void displayInfo(){
-      cout << "My name is " << name << endl;
 
-
-
-};
-class Course{
-   public:
-   string courseName;
-   int courseId;
-   int courseMark;
-
-   Course(string courseName,string courseId,int courseMark){
-    this->courseName = courseName;
-    this->courseId = courseId;
-    this->courseMark = courseMark;
-   }
+    float average(){
+     float TotalMark=0;
+     int totalSum=0;
+      for(int i=0;i<courseOffer;i++){
+        TotalMark += courses[i].courseMark * courses[i].coef;
+        totalSum += courses[i].coef;
+      }
+      return TotalMark/totalSum;
+    }
 
 };
 
 int main()
 {
-    cout << "===REPORT CARD===" << endl << endl;
-    Student student1("Atabe",2);
-    student1.displayInfo();
+    int numberOfStudents=0;
+    cout << "\t===REPORT CARD===" << endl;
+    cout << "Enter the number of students:";
+    cin >> numberOfStudents;
+    cout << "\n";
+    Student* students = new Student[numberOfStudents];
+
+
+    for(int i=0;i<numberOfStudents;i++){
+        cout << "Student " << i+1 <<endl;
+        cout << "Enter your name:";
+        cin >> students[i].name;
+        cout << "Enter the number of course you wish to offer:";
+         cout << "Enter your gender:";
+        cin >> students[i].gender;
+        students[i].courses = new Course[students[i].courseOffer];
+        cout << "\n";
+        for(int j=0;j < students[i].courseOffer;j++){
+            cout << "course" << j+1 << endl;
+            cout << "Enter name for course:";
+            cin >> students[i].courses[j].courseName;
+            cout << students[i].courses[j].courseName;
+            cout << "Enter it's coefficient:";
+            cin >> students[i].courses[j].coef;
+            cout << "Enter mark for the course:";
+            cin >> students[i].courses[j].courseMark;
+            cout << "\n";
+        }
+        cout << "\n";
+    }
+      cout << "Name\tGender\tNumber of Course\tAverage\n";
+     for(int i=0;i < numberOfStudents;i++){
+           cout<< students[i].name << "\t"<< students[i].gender<<"\t"<< students[i].courseOffer<<"\t"<<students[i].average();
+           cout << "\n";
+        }
     return 0;
 }
